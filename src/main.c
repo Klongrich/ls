62489699eb7fccfb5cfdd145aec8f11ead491148
klongrich <longrichk@gmail.com>
2026-03-19 18:41:42 -0400

    adding is_flag check to increment to args passed

--- src/main.c
+++ src/main.c
@@ -52,6 +52,36 @@ void read_dir(){
 }
 
 
+
+int is_flag(char *str) {
+	int i;
+
+	i = 0;
+	if (str[i] == '-') {
+		i++;
+		if (!str[i])
+			return (0);
+	} else {
+		return (0);
+	}
+
+	while (str[i]) {
+		if (str[i] == 'a')
+			i++;
+		else if (str[i] == 't')
+			i++;
+		else if (str[i] == 'l')
+			i++;
+		else if (str[i] == 'r')
+			i++;
+		else if (str[i] == 'R')
+			i++;
+		else
+			return(0);
+	}
+	return (1);
+}
+
 void run_recur(char **argv, t_flags *flags, int i) {
 	while (argv[i]) {
 		printf("%s\n", argv[i]);
@@ -112,6 +142,8 @@ int 	main(int argc, char **argv){
 				} else {
 					
 					if(flags.recur) {
+						if (is_flag(argv[i]))
+							i++;
 						run_recur(argv, &flags, i);
 						return(0);
 					}
