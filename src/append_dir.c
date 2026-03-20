1896ae89d814c687591a17771bdfa5d1504db146
klongrich <longrichk@gmail.com>
2026-03-20 18:06:37 -0400

    adding update for if ../ or ../../ is passed

--- src/append_dir.c
+++ src/append_dir.c
@@ -3,15 +3,21 @@
 char	**append_dir(char *dir, char **files_or_dirs) {
 	int i;
 	char **res;
+	char *dir_;
 
 	i = 0;
+	dir_ = dir;
 	res = (char **)malloc(sizeof(char *) * get_size(files_or_dirs) + 1);
 	if(!res) {
 		printf("error\n");
 	}	
+	if (!check_end(dir))
+		dir_ = ft_strjoin(dir, "/");
 	while(files_or_dirs[i]) {
-		res[i] = ft_strjoin(dir_, files_or_dirs[i]);
+		res[i] = ft_strjoin(dir, files_or_dirs[i]);
 		i++;
 	}
+	if (!check_end(dir))
+		free(dir_);
 	return(res);
 }
