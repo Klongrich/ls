534d30dadb4692785f92350dc8c91b73ae1a7031
klongrich <longrichk@gmail.com>
2026-03-21 13:14:07 -0400

    added -Ra support for directories passed as arguments

--- src/append_dir.c
+++ src/append_dir.c
@@ -2,9 +2,11 @@
 
 char	**append_dir(char *dir, char **files_or_dirs) {
 	int i;
+	int j;
 	char **res;
 	char *dir_;
 
+	j = 0;
 	i = 0;
 	dir_ = dir;
 	res = (char **)malloc(sizeof(char *) * get_size(files_or_dirs) + 1);
@@ -14,8 +16,16 @@ char	**append_dir(char *dir, char **files_or_dirs) {
 	if (!check_end(dir))
 		dir_ = ft_strjoin(dir, "/");
 	while(files_or_dirs[i]) {
-		res[i] = ft_strjoin(dir_, files_or_dirs[i]);
+		while ((!ft_strcmp(files_or_dirs[i], ".") || !ft_strcmp(files_or_dirs[i], ".."))) {
+			i++;
+			if (!files_or_dirs[i])
+				break;
+		}
+		if(!files_or_dirs[i])
+			break;
+		res[j] = ft_strjoin(dir_, files_or_dirs[i]);
 		i++;
+		j++;
 	}
 	if (!check_end(dir))
 		free(dir_);
