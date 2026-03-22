74d5921da82f78ea823f120bb679d5a13797e1fb
klongrich <longrichk@gmail.com>
2026-03-22 17:58:33 -0400

    removing : from print formatting

--- src/print_long_format.c
+++ src/print_long_format.c
@@ -5,7 +5,7 @@ void	print_long_format(char **all_files_or_dirs) {
 
 	k = 0;
 	while (all_files_or_dirs[k]) {
-		printf("%s:\n", all_files_or_dirs[k]);
+		printf("%s\n", all_files_or_dirs[k]);
 		k++;
 	}
 
