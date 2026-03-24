20a97a9c0ac608d017d257375ef61fab80c6ce71
klongrich <longrichk@gmail.com>
2026-03-24 14:55:47 -0400

    removing unnecessary checks causing error on -Rta

--- src/append_single_dir.c
+++ src/append_single_dir.c
@@ -7,9 +7,6 @@ char	*append_single_dir(char *dir, char *files_or_dir) {
 	dir_ = dir;	
 	if (!check_end(dir))
 		dir_ = ft_strjoin(dir, "/");
-	if ((!ft_strcmp(files_or_dir, ".") || !ft_strcmp(files_or_dir, ".."))) {
-			return(files_or_dir);
-	}
 	res = ft_strjoin(dir_, files_or_dir);
 	if (!check_end(dir))
 		free(dir_);
