d746d9ca7f99a81661ba2a0ec32f56a5978673fc
klongrich <longrichk@gmail.com>
2026-03-27 13:56:51 -0400

    refactoring t_lengths variable name

--- src/print_long_format.c
+++ src/print_long_format.c
@@ -1,12 +1,13 @@
 #include "../inc/ft_ls.h"
 
 void	print_long_format(char **all_files_or_dirs) {
-	t_lengths	file_or_dir_info;
+	t_lengths	formatting_lengths;
 	int k;
 
 	k = 0;
-	file_or_dir_info = get_total(all_files_or_dirs);
-	ft_printf("total %d\n", file_or_dir_info.count);
+	formatting_lengths = get_formatting_lengths(all_files_or_dirs);
+	printf("total %d\n", formatting_lengths.count);
+	//print_t_lengths(formatting_lengths);
 	while (all_files_or_dirs[k]) {
 		printf("%s\n", all_files_or_dirs[k]);
 		k++;
