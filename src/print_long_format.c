f1d817d02dbd7c37a00789023105aa9fe339b77b
klongrich <longrichk@gmail.com>
2026-03-27 18:09:50 -0400

    printing file or directory name instead of path

--- src/print_long_format.c
+++ src/print_long_format.c
@@ -4,6 +4,7 @@ void	print_long_format(char **all_files_or_dirs) {
 	t_lengths	formatting_lengths;
 	struct stat	statbuff;
 	int k;
+	char *temp;
 
 	k = 0;
 	formatting_lengths = get_formatting_lengths(all_files_or_dirs);
@@ -15,7 +16,10 @@ void	print_long_format(char **all_files_or_dirs) {
 		print_name_and_grid(formatting_lengths, statbuff);
 		ft_printf("%*d", formatting_lengths.size, (int)statbuff.st_size);
 		print_last_time_modified(all_files_or_dirs[k]);
-		printf("%s\n", all_files_or_dirs[k]);
+		
+		temp = get_name_from_path(all_files_or_dirs[k]);
+		printf("%s\n", temp);
+		free(temp);
 		k++;
 	}
 
