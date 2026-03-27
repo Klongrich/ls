f2971349fd2dd7482f14176978dfc1985eb00861
klongrich <longrichk@gmail.com>
2026-03-27 16:38:15 -0400

    printing user name from pwd with proper formatting for -l flag

--- src/print_name_and_grid.c
+++ src/print_name_and_grid.c
@@ -1,8 +1,12 @@
 #include "../inc/ft_ls.h"
 
 void	print_name_and_grid(t_lengths formatting_info, struct stat statbuff) {
-	//struct passwd	*pwd;
+	struct passwd	*pwd;
 	//struct group	*grp;
 
 	ft_printf("%*d ", formatting_info.links, statbuff.st_nlink); 
+	if ((pwd = getpwuid(statbuff.st_uid)) != NULL)
+		ft_printf("%*s", formatting_info.name + 2, pwd->pw_name);
+	else
+		ft_printf("%*d", formatting_info.name + 2, statbuff.st_uid);
 }
