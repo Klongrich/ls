536ccc6c025b07179efaa82208abd2723964d3d2
klongrich <longrichk@gmail.com>
2026-03-27 18:10:37 -0400

    removing comment used for debugging

--- src/print_long_format.c
+++ src/print_long_format.c
@@ -9,7 +9,6 @@ void	print_long_format(char **all_files_or_dirs) {
 	k = 0;
 	formatting_lengths = get_formatting_lengths(all_files_or_dirs);
 	printf("total %d\n", formatting_lengths.count);
-	//print_t_lengths(formatting_lengths);
 	while (all_files_or_dirs[k]) {
 		lstat(all_files_or_dirs[k], &statbuff);
 		print_permissions(statbuff);
