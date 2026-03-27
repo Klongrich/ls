14d1b7943c9ca3464949ab66f7e9ba55fd34e3af
klongrich <longrichk@gmail.com>
2026-03-27 13:24:28 -0400

    getting column size for group name for -l flag formatting

--- src/set_column_size.c
+++ src/set_column_size.c
@@ -2,6 +2,7 @@
 
 t_lengths	set_column_sizes(t_lengths formatting_info, struct stat statbuf) {
 	struct passwd	*pwd;
+	struct group	*grp;
 
 	if ((pwd = getpwuid(statbuf.st_uid)))
 		(ft_strlen(pwd->pw_name) > formatting_info.name) 
@@ -9,5 +10,12 @@ t_lengths	set_column_sizes(t_lengths formatting_info, struct stat statbuf) {
 	else
 		(ft_numlen(statbuf.st_uid) > formatting_info.name)
 		? formatting_info.name = ft_numlen(statbuf.st_uid) : 0;
+
+	if ((grp = getgrgid(statbuf.st_gid)) != NULL)
+		(ft_strlen(grp->gr_name) > formatting_info.group)
+		? formatting_info.group = ft_strlen(grp->gr_name) : 0;
+	else
+		(ft_numlen(statbuf.st_gid) > formatting_info.group)
+		? formatting_info.group = ft_numlen(statbuf.st_gid) : 0;
 	return (formatting_info);
 }
