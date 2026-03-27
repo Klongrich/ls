a9042a91a86dcacbe3ba7205bca5a847c9a18d32
klongrich <longrichk@gmail.com>
2026-03-27 12:31:52 -0400

    getting block total then printing it

--- src/print_long_format.c
+++ src/print_long_format.c
@@ -1,9 +1,12 @@
 #include "../inc/ft_ls.h"
 
 void	print_long_format(char **all_files_or_dirs) {
+	t_lengths	file_or_dir_info;
 	int k;
 
 	k = 0;
+	file_or_dir_info = get_total(all_files_or_dirs);
+	ft_printf("total %d\n", file_or_dir_info.count);
 	while (all_files_or_dirs[k]) {
 		printf("%s\n", all_files_or_dirs[k]);
 		k++;
