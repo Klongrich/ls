f9ca4d88b7042abe56b25dd05c31c396bdb66c02
klongrich <longrichk@gmail.com>
2026-03-27 16:44:10 -0400

    adding printing for group name of file or directory for -l flag

--- src/print_name_and_grid.c
+++ src/print_name_and_grid.c
@@ -2,11 +2,16 @@
 
 void	print_name_and_grid(t_lengths formatting_info, struct stat statbuff) {
 	struct passwd	*pwd;
-	//struct group	*grp;
+	struct group	*grp;
 
 	ft_printf("%*d ", formatting_info.links, statbuff.st_nlink); 
 	if ((pwd = getpwuid(statbuff.st_uid)) != NULL)
 		ft_printf("%*s", formatting_info.name + 2, pwd->pw_name);
 	else
 		ft_printf("%*d", formatting_info.name + 2, statbuff.st_uid);
+	if ((grp = getgrgid(statbuff.st_gid)) != NULL)
+		ft_printf("%*s", formatting_info.group + 2, grp->gr_name);
+	else
+		ft_printf("%*d", formatting_info.group + 2, statbuff.st_gid);
+	
 }
