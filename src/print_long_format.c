e382c92091e966166cb88ff96d329a7297632008
klongrich <longrichk@gmail.com>
2026-03-30 19:35:51 -0400

    removing comments, added example using makedev to print major and minor numbers as one hexadecimal value

--- src/print_long_format.c
+++ src/print_long_format.c
@@ -4,7 +4,6 @@ void	print_long_format(char **all_files_or_dirs) {
 	t_lengths	formatting_lengths;
 	struct stat	statbuff;
 	int k;
-	//char *temp;
 
 	k = 0;
 	formatting_lengths = get_formatting_lengths(all_files_or_dirs);
@@ -14,15 +13,13 @@ void	print_long_format(char **all_files_or_dirs) {
 		print_permissions(statbuff);
 		print_name_and_grid(formatting_lengths, statbuff);
 		if (S_ISCHR(statbuff.st_mode) || S_ISBLK(statbuff.st_mode)) {
+			//printf("%#010x", makedev(major(statbuff.st_rdev), minor(statbuff.st_rdev)));
 			ft_printf("%*d,", 5, major(statbuff.st_rdev));
 			ft_printf("%*d",5, minor(statbuff.st_rdev));
 		} else
 			ft_printf("%*d", formatting_lengths.size, (int)statbuff.st_size);
 		print_last_time_modified(statbuff);
 		print_file_name(all_files_or_dirs[k]);
-		//temp = get_name_from_path(all_files_or_dirs[k]);
-		//ft_printf("%s\n", temp);
-		//free(temp);
 		k++;
 	}
 
