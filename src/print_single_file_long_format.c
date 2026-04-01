0aa2edd23051aca8710c437d1da9cae220084a86
klongrich <longrichk@gmail.com>
2026-04-01 15:25:49 -0400

    fixing passing single file with path to -l format to print file path instead of file name

--- src/print_single_file_long_format.c
+++ src/print_single_file_long_format.c
@@ -3,7 +3,6 @@
 void	print_single_file_long_format(char *file_path) {
 	t_lengths	formatting_lengths;
 	struct stat	statbuff;
-	char *temp;
 
 	formatting_lengths = get_single_file_formatting_lengths(file_path);
 
@@ -13,8 +12,12 @@ void	print_single_file_long_format(char *file_path) {
 	ft_printf("%*d", formatting_lengths.size, (int)statbuff.st_size);
 	print_last_time_modified(statbuff);
 
-	temp = get_name_from_path(file_path);
-	printf("%s\n", temp);
-	free(temp);
+	if(file_path[0] != '/') {
+		file_path++;
+		file_path++;
+		printf("%s\n", file_path);
+	} else {
+		printf("%s\n", file_path);
+	}
 }
 
