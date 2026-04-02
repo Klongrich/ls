45d8589b98b6390827e4d2d5feb5270e0a9edd45
klongrich <longrichk@gmail.com>
2026-04-02 15:18:54 -0400

    getting formatting length for hex number returned from makedev

--- src/get_formatting_lengths.c
+++ src/get_formatting_lengths.c
@@ -2,19 +2,30 @@
 
 t_lengths	get_formatting_lengths(char **file_or_dir_paths) {
 	int	i;
+	int 	num;
+	int	hex_len;
+	char	hex[20];
 	struct stat statbuf;
 	t_lengths	formatting_info;
 
 	formatting_info = initialize_t_lengths();
 	i = 0;
-
+	hex_len = 0;
 	while (file_or_dir_paths[i]) {
 		lstat(file_or_dir_paths[i], &statbuf);
 		if(ft_numlen(statbuf.st_nlink) > formatting_info.links)
 			formatting_info.links = ft_numlen(statbuf.st_nlink);
 		
 		formatting_info = set_column_sizes(formatting_info, statbuf);
-
+		
+		hex_len = 0;
+		if (S_ISCHR(statbuf.st_mode) || S_ISBLK(statbuf.st_mode)) {
+			num = makedev(major(statbuf.st_rdev), minor(statbuf.st_rdev));
+			decimalToHex(num, hex);
+			hex_len = ft_strlen(hex) + 2;
+		}
+		if (hex_len > formatting_info.size)
+			formatting_info.size = hex_len;
 		if(ft_numlen(statbuf.st_size) > formatting_info.size)
 			formatting_info.size = ft_numlen(statbuf.st_size);
 		i++;
