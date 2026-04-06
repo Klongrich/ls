c39bb1ff984f6496d2aca7498d677023f2c0b91d
klongrich <longrichk@gmail.com>
2026-04-06 15:14:21 -0400

    removing comments

--- read_dir/read_dir.c
+++ read_dir/read_dir.c
@@ -1,14 +1,9 @@
 #include "../inc/ft_ls.h"
 
 char 	**read_dir(char *dir_path, t_flags *flags){
-///	DIR	*dir;
-//	struct dirent *dp;
 	int repo_or_file_count;
 	char **all_files_or_directories;
-//	char *temp;
-//	int i;
 
-//	i = 0;
 	repo_or_file_count = get_repo_or_file_count(dir_path, flags->a, flags->l);	
 	if (!repo_or_file_count) {
 		return(0);
@@ -17,47 +12,11 @@ char 	**read_dir(char *dir_path, t_flags *flags){
 		all_files_or_directories = (char **)malloc(sizeof(char *) * repo_or_file_count + 1);
 		if (!all_files_or_directories)
 			printf("Error mallocing\n");
-
 		if(flags->a) 
 			read_dir_with_hidden_files(all_files_or_directories, dir_path, flags);
 		else
 			read_dir_standard(all_files_or_directories, dir_path, flags);
 
-		/*
-		dir = opendir(dir_path);
-		while ((dp = readdir(dir))) {
-			if(!flags->a) {
-				if (!(dp->d_name[0] == '.')){
-					if (flags->t || flags->l) {
-						all_files_or_directories[i] = (char *)malloc(sizeof(char) * MAX_PATH_LENGTH);
-						temp = append_single_dir(dir_path, dp->d_name);
-						all_files_or_directories[i] = ft_strcpy(all_files_or_directories[i], temp);
-						free(temp); 
-					} else {
-						all_files_or_directories[i] = (char *)malloc(sizeof(char) * MAX_FILE_LENGTH);
-						all_files_or_directories[i] = ft_strcpy(all_files_or_directories[i], dp->d_name);
-					}
-					i++;		
-				}
-			} else {
-				if(flags->t || flags->l) {
-					all_files_or_directories[i] = (char *)malloc(sizeof(char) * MAX_PATH_LENGTH);
-					temp = append_single_dir(dir_path, dp->d_name);
-					all_files_or_directories[i] = ft_strcpy(all_files_or_directories[i], temp);
-					free(temp);
-				} else {
-					all_files_or_directories[i] = (char *)malloc(sizeof(char) * MAX_FILE_LENGTH);
-					all_files_or_directories[i] = ft_strcpy(all_files_or_directories[i], dp->d_name);
-				}
-				i++;
-			}
-		}
-
-		all_files_or_directories[i] = 0;
-		closedir(dir);
-
-		*/
-		
 		if (flags->t) 
 			merge_time_sort(all_files_or_directories, 0, get_size(all_files_or_directories) - 1, flags->r);
 		else 
