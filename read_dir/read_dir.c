9a261ff69d1e2a9852b2fd96f656c5415b22bf56
klongrich <longrichk@gmail.com>
2026-04-06 15:21:33 -0400

    adding handel_print function

--- read_dir/read_dir.c
+++ read_dir/read_dir.c
@@ -1,5 +1,14 @@
 #include "../inc/ft_ls.h"
 
+void	handle_printing(char **all_files_or_directories, t_flags *flags) {
+	if(flags->l)
+		print_long_format(all_files_or_directories);    
+	if (flags->l || flags->t) 
+		cut_file_path_from_files_or_dirs(all_files_or_directories);
+	if(!flags->l)
+		print_list(all_files_or_directories);
+}
+
 char 	**read_dir(char *dir_path, t_flags *flags){
 	int repo_or_file_count;
 	char **all_files_or_directories;
@@ -8,10 +17,7 @@ char 	**read_dir(char *dir_path, t_flags *flags){
 	if (!repo_or_file_count) {
 		return(0);
 	} else {
-
 		all_files_or_directories = (char **)malloc(sizeof(char *) * repo_or_file_count + 1);
-		if (!all_files_or_directories)
-			printf("Error mallocing\n");
 		if(flags->a) 
 			read_dir_with_hidden_files(all_files_or_directories, dir_path, flags);
 		else
@@ -21,14 +27,7 @@ char 	**read_dir(char *dir_path, t_flags *flags){
 			merge_time_sort(all_files_or_directories, 0, get_size(all_files_or_directories) - 1, flags->r);
 		else 
 			merge_sort2(all_files_or_directories, 0, get_size(all_files_or_directories) - 1, flags->r);
-
-		if(flags->l)
-			print_long_format(all_files_or_directories);	
-		if (flags->l || flags->t) 
-			cut_file_path_from_files_or_dirs(all_files_or_directories);
-		if(!flags->l)
-			print_list(all_files_or_directories);
-
+		handle_printing(all_files_or_directories, flags);
 		return(all_files_or_directories);
 	}
 }
