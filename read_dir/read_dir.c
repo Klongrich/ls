de0a0d6feead77861ed97f5358c79773bc842681
klongrich <longrichk@gmail.com>
2026-04-08 13:17:38 -0400

    adding logic checking if files are passed from argv

--- read_dir/read_dir.c
+++ read_dir/read_dir.c
@@ -4,7 +4,7 @@ void	handel_printing(char **all_files_or_directories, t_flags *flags) {
 	if(flags->l)
 		print_long_format(all_files_or_directories, flags);
 	if(flags->color & !flags->l)
-		print_color(all_files_or_directories);    
+		print_color(all_files_or_directories, 0);    
 	if (flags->l || flags->t || flags->color) 
 		cut_file_path_from_files_or_dirs(all_files_or_directories);
 	if(!flags->l & !flags->color)
