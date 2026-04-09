af62d8e06495e4b89a7119f3866dc7ccec22bff9
klongrich <longrichk@gmail.com>
2026-04-09 15:08:05 -0400

    handling special character _ comparison when sorting

--- sort/compare_strs_lowercase.c
+++ sort/compare_strs_lowercase.c
@@ -5,17 +5,27 @@ int	compare_strs_lowercase(char *str1, char *str2){
 	char *lower_case_all_characters_one;
 	char *lower_case_all_characters_two;
 	int diff;
+	int spec;
 
-	count = 0; 
+	count = 0;
+	spec = 0; 
 	lower_case_all_characters_one = (char *)malloc(sizeof(char) * ft_strlen(str1));
 	lower_case_all_characters_two = (char *)malloc(sizeof(char) * ft_strlen(str2)); 
 	while (str1[count]) {
-		lower_case_all_characters_one[count] = ft_tolower(str1[count]);
+		spec = 0;
+		if (str1[count] == '_') {
+			spec = 52;
+		}
+		lower_case_all_characters_one[count] = ft_tolower(str1[count] - spec);
 		count++;
 	}
 	count = 0;
 	while(str2[count]){
-		lower_case_all_characters_two[count] = ft_tolower(str2[count]);
+		spec = 0;
+		if (str2[count] == '_') {
+			spec = 52;
+		}
+		lower_case_all_characters_two[count] = ft_tolower(str2[count] - spec);
 		count++;
 	}
 
