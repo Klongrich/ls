b5499d2e0662f2d887863ebb6cb2b9796973b695
klongrich <longrichk@gmail.com>
2026-04-09 13:09:00 -0400

    printing the date and year if last time modified was over 6 months ago

--- print/print_last_time_modified.c
+++ print/print_last_time_modified.c
@@ -1,10 +1,42 @@
 #include "../inc/ft_ls.h"
 
+void	print_date_year(char *times) {
+	char *year;
+	char *date;
+	int i;
+               
+	i = 0;  
+	ft_putchar(' ');
+	date = times + 4;
+	year = times + 20;
+	while(i < 7) {
+		ft_putchar(date[i]);    
+		i++;
+	}
+	ft_putchar(' ');
+	i = 0;
+	while (i < 4) {
+		ft_putchar(year[i]);
+		i++;
+	}
+	i = 0;
+	while (i < 1) {
+		ft_putchar(' ');
+		i++;
+	}
+}
+
 void		print_last_time_modified(struct stat statbuff)
 {
-	char		*times;
+	time_t	now;
+	char	*times;
 
+	now = time(NULL);
 	times = ctime(&statbuff.st_mtime);
-	times += 3;
-	ft_printf("%.*s ", 13, times);
+	if ((now - statbuff.st_mtime) > (182 * 24 * 60 * 80)) {
+		print_date_year(times);
+	} else {
+		times += 3;
+		ft_printf("%.*s ", 13, times);
+	}
 }
