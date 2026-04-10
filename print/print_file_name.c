b7ca516743170b58f9aa37b289d4ff160d9d75f5
klongrich <longrichk@gmail.com>
2026-04-10 18:13:08 -0400

    adding support context awareness for -g flag

--- print/print_file_name.c
+++ print/print_file_name.c
@@ -21,7 +21,7 @@ void	print_file_name(char *filepath, int is_single_file, t_flags *flags)
 		ft_printf("%s -> %s\n", name, buff);
 	}
 	else {
-		if (flags->color)
+		if (flags->color & isatty(STDOUT_FILENO))
 			print_color_single(filepath, 0, 1);
 		else
 			ft_printf("%s\n", name);
