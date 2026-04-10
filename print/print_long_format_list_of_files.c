287ae3fb4a9b6d3eb3844625036b729302eb25bf
klongrich <longrichk@gmail.com>
2026-04-10 18:13:53 -0400

    supporting context awareness for -G

--- print/print_long_format_list_of_files.c
+++ print/print_long_format_list_of_files.c
@@ -20,7 +20,7 @@ void	print_long_format_files_from_args(char **all_files, t_flags *flags) {
 			print_last_time_accessed(statbuff);
 		else
 			print_last_time_modified(statbuff);
-		if (flags->color) {
+		if (flags->color & isatty(STDOUT_FILENO)) {
 			print_color_single(all_files_appended[k], 1, 1);
 		} else {
 			if(all_files_appended[k][0] != '/') {
