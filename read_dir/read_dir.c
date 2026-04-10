319d364acb0b748233ab1529062bb59bbdbbab22
klongrich <longrichk@gmail.com>
2026-04-10 18:09:31 -0400

    making updates for -f flag, printing color strictly to terminal

--- read_dir/read_dir.c
+++ read_dir/read_dir.c
@@ -3,11 +3,11 @@
 void	handel_printing(char **all_files_or_directories, t_flags *flags) {
 	if(flags->l)
 		print_long_format(all_files_or_directories, flags);
-	if(flags->color & !flags->l)
+	if(flags->color & !flags->l & isatty(STDOUT_FILENO))
 		print_color(all_files_or_directories, 0, 0);    
 	if (flags->l || flags->t || flags->color) 
 		cut_file_path_from_files_or_dirs(all_files_or_directories);
-	if(!flags->l & !flags->color)
+	if(!flags->l & !flags->color || (flags->color & !isatty(STDOUT_FILENO) & !flags->l))
 		print_list(all_files_or_directories);
 }
 
@@ -15,21 +15,21 @@ char 	**read_dir(char *dir_path, t_flags *flags){
 	int repo_or_file_count;
 	char **all_files_or_directories;
 
-	repo_or_file_count = get_repo_or_file_count(dir_path, flags->a, flags->l);	
+	repo_or_file_count = get_repo_or_file_count(dir_path, flags->a, flags->l, flags->f);	
 	if (!repo_or_file_count) {
 		return(0);
 	} else {
 		all_files_or_directories = (char **)malloc(sizeof(char *) * repo_or_file_count + 1);
-		if(flags->a) 
+		if(flags->a || flags->f) 
 			read_dir_with_hidden_files(all_files_or_directories, dir_path, flags);
 		else
-			read_dir_standard(all_files_or_directories, dir_path, flags);
-		if (flags->t) {
+			read_dir_standard(all_files_or_directories, dir_path, flags);	
+		 if (flags->t & !flags->f) {
 			if(flags->u) 
 				merge_last_time_accessed_sort(all_files_or_directories, 0, get_size(all_files_or_directories) - 1, flags->r);
 			else
 				merge_time_sort(all_files_or_directories, 0, get_size(all_files_or_directories) - 1, flags->r);
-		} else 
+		} else if (!flags->f)
 			merge_sort2(all_files_or_directories, 0, get_size(all_files_or_directories) - 1, flags->r);
 		handel_printing(all_files_or_directories, flags);
 		return(all_files_or_directories);
