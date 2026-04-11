e3629f8140c843a7b8506dad1a22409228634f2e
klongrich <longrichk@gmail.com>
2026-04-11 14:11:39 -0400

    refactored print_single_file_long_format

--- print/print_single_file_long_format.c
+++ print/print_single_file_long_format.c
@@ -1,11 +1,20 @@
 #include "../inc/ft_ls.h"
 
+void	put_file_name(char *file_path, t_flags *flags) {
+	if(file_path[0] != '/') {
+		file_path++;
+		file_path++;
+		printf("%s\n", file_path);
+	} else {
+		print_file_name(file_path, 1, flags);
+	}
+}
+
 void	print_single_file_long_format(char *file_path, t_flags *flags) {
 	t_lengths	formatting_lengths;
 	struct stat	statbuff;
 
 	formatting_lengths = get_single_file_formatting_lengths(file_path);
-
 	lstat(file_path, &statbuff);
 	print_permissions(statbuff, file_path);
 	print_name_and_grid(formatting_lengths, statbuff);
@@ -13,22 +22,14 @@ void	print_single_file_long_format(char *file_path, t_flags *flags) {
 	if(flags->t & flags->u)
 		print_last_time_accessed(statbuff);
 	else
-		print_last_time_modified(statbuff);
-	
+		print_last_time_modified(statbuff);	
 	if(flags->color & isatty(STDOUT_FILENO)) {
 		print_color_single(file_path, 1, 1);
 	} else {
 		if(check_files_link(file_path)) {
 			print_file_name(file_path, 1, flags);
 		} else {
-			if(file_path[0] != '/') {
-				file_path++;
-				file_path++;
-				printf("%s\n", file_path);
-			} else {
-				print_file_name(file_path, 1, flags);
-			}
+			put_file_name(file_path, flags);
 		}
 	}
 }
-
