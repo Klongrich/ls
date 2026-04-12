b074fc4815d152559b86a4eadedd24ca42041825
klongrich <longrichk@gmail.com>
2026-04-11 21:58:53 -0400

    adding space

--- parse_flags/set_flag_values.c
+++ parse_flags/set_flag_values.c
@@ -6,7 +6,6 @@ int	print_invalid_flag_error() {
 }
 
 int	set_flag_value(char c, t_flags *flags) {
-
 	if (c == 'l')
 		flags->l = 1;
 	else if (c == 'r')
@@ -23,7 +22,7 @@ int	set_flag_value(char c, t_flags *flags) {
 		flags->u = 1;
 	else if (c  == 'f')
 		flags->f = 1;
-	else if (c == 'd')
+	else if	(c == 'd')
 		flags->d = 1;
 	else
 		return(print_invalid_flag_error());
