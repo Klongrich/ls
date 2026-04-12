217bc3472eef7483a98ffd3d3dd6d70e8482f485
klongrich <longrichk@gmail.com>
2026-04-11 21:59:57 -0400

    adding support for -d with no arguments passed

--- run/run_with_no_files_or_repos_passed.c
+++ run/run_with_no_files_or_repos_passed.c
@@ -3,9 +3,11 @@
 void	run_with_no_files_or_repos_passed(t_flags *flags) {
 	char **temp;
 	char **temp3;
-	char **temp4;	
+	char **temp4;
 
-	if(flags->recur) {
+	if (flags->d) {	
+		ft_printf(".\n");
+	} else if(flags->recur) {
 		temp3 = read_dir(".", flags);
 		temp4 = append_dir("./", temp3);;
 		recur(temp4, flags);
