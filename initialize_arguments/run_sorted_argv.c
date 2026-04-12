0e9a41afea4bbd77a352da5f4cb99de02694259b
klongrich <longrichk@gmail.com>
2026-04-12 02:19:30 -0400

    preventing -R when -d is passed

--- initialize_arguments/run_sorted_argv.c
+++ initialize_arguments/run_sorted_argv.c
@@ -4,7 +4,7 @@ void	run_sorted_argv(char **sorted_files, char **sorted_dirs, int number_of_inva
 	int number_of_valid_args;	
 
 	number_of_valid_args = get_size(sorted_files) + get_size(sorted_dirs);
-	if(flags->recur) {
+	if(flags->recur && !flags->d) {
 		run_recursion(sorted_files, sorted_dirs, flags, number_of_invalid_args);
 	} else {
 		if(number_of_valid_args == 1 && number_of_invalid_args == 0)
