a720bbcfca74228458fb8c5a9882681b3749a191
klongrich <longrichk@gmail.com>
2026-04-13 05:26:49 -0400

    refactored decimal_to_hex.c

--- src/decimal_to_hex.c
+++ src/decimal_to_hex.c
@@ -1,24 +1,25 @@
 #include "../inc/ft_ls.h"
 
 void decimalToHex(int n, char hexString[]) {
-    char hexChars[] = "0123456789ABCDEF";
-    char temp[20];
-    int i = 0;
+	char *hexChars;
+	char temp[20];
+	int i;
+	int j;
 
-    if (n == 0) {
-        hexString[0] = '0';
-        hexString[1] = '\0';
-        return;
-    }
-
-    while (n > 0) {
-        temp[i++] = hexChars[n % 16];
-        n /= 16;
-    }
-
-    int j = 0;
-    while (i > 0) {
-        hexString[j++] = temp[--i];
-    }
-    hexString[j] = '\0';
+	j = 0;
+	i = 0;
+	hexChars = "0123456789ABCDEF";
+	if (n == 0) {
+        	hexString[0] = '0';
+        	hexString[1] = '\0';
+        	return;
+    	}
+	while (n > 0) {
+        	temp[i++] = hexChars[n % 16];
+		n /= 16;
+	}
+	while (i > 0) {
+        	hexString[j++] = temp[--i];
+    	}
+    	hexString[j] = '\0';
 }
