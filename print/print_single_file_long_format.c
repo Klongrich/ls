a29c65158de0edec6fdb356cb7853f66957f6f42
klongrich <longrichk@gmail.com>
2026-04-12 21:56:11 -0400

    adding check for single file or repo passed on -ldG

--- print/print_single_file_long_format.c
+++ print/print_single_file_long_format.c
@@ -16,6 +16,14 @@ void	put_file_name(char *file_path, t_flags *flags) {
 	}
 }
 
+void	print_dl_single(char *file_path, t_flags *flags) {
+	if(is_file(file_path, flags) && file_path[0] == '.') {
+		file_path++;
+		file_path++;
+	}	
+	print_color_d_flag(file_path, flags);
+}
+
 void	print_single_file_long_format(char *file_path, t_flags *flags) {
 	t_lengths	formatting_lengths;
 	struct stat	statbuff;
@@ -30,7 +38,10 @@ void	print_single_file_long_format(char *file_path, t_flags *flags) {
 	else
 		print_last_time_modified(statbuff);	
 	if(flags->color & isatty(STDOUT_FILENO)) {
-		print_color_single(file_path, 1, 1);
+		if (flags->d) 
+			print_dl_single(file_path, flags);
+		else
+			print_color_single(file_path, 1, 1);
 	} else {
 		if(check_files_link(file_path)) {
 			print_file_name(file_path, 1, flags);
