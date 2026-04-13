d2ba684ad683b8e6944f84a90924650cc700bd73
klongrich <longrichk@gmail.com>
2026-04-13 05:13:47 -0400

    refactored last_time_accessed_sort

--- sort/last_time_accessed_sort.c
+++ sort/last_time_accessed_sort.c
@@ -1,13 +1,33 @@
 #include "../inc/ft_ls.h"
 
+void	compare_time_reverse_a(struct stat file_one, struct stat file_two, int j, char **str) {
+	if(file_one.st_atime == file_two.st_atime) {
+		if(file_one.st_atimespec.tv_nsec > file_two.st_atimespec.tv_nsec)
+			bubble_swap(&str[j], &str[j + 1]);
+	} else {
+		if (file_one.st_atime > file_two.st_atime)
+			bubble_swap(&str[j], &str[j + 1]);
+	}
+}
+
+void	compare_time_normal_a(struct stat file_one, struct stat file_two, int j, char **str) {
+	if (file_one.st_atime == file_two.st_atime) {
+		if (file_one.st_atimespec.tv_nsec < file_two.st_atimespec.tv_nsec) {
+			bubble_swap(&str[j], &str[j + 1]);
+		}
+	} else {
+		if (file_one.st_atime < file_two.st_atime) {
+			bubble_swap(&str[j], &str[j + 1]);
+		}
+	}
+}
+
 char	**last_time_accessed_sort(char **str, int r) {
-	
 	struct stat file_one;
 	struct stat file_two;
 	int i;
 	int j;
 	int size;
-	char temp[MAX_FILE_LENGTH];
 
 	size = get_size(str);
 	i = 0;
@@ -18,33 +38,9 @@ char	**last_time_accessed_sort(char **str, int r) {
 			lstat(str[j], &file_one);
 			lstat(str[j + 1], &file_two);
 			if (r) {
-				if(file_one.st_atime == file_two.st_atime) {
-					if(file_one.st_atimespec.tv_nsec > file_two.st_atimespec.tv_nsec) {
-						ft_strcpy(temp, str[j]);
-						ft_strcpy(str[j], str[j + 1]);
-						ft_strcpy(str[j + 1], temp);
-					}
-				} else {
-					if (file_one.st_atime > file_two.st_atime) {
-						ft_strcpy(temp, str[j]);
-						ft_strcpy(str[j], str[j + 1]);
-						ft_strcpy(str[j + 1], temp);
-					}
-				}
+				compare_time_reverse_a(file_one, file_two, j, str);
 			} else {
-				if (file_one.st_atime == file_two.st_atime) {
-					if (file_one.st_atimespec.tv_nsec < file_two.st_atimespec.tv_nsec) {
-						ft_strcpy(temp, str[j]);
-						ft_strcpy(str[j], str[j + 1]);
-						ft_strcpy(str[j + 1], temp);
-					}
-				} else {
-					if (file_one.st_atime < file_two.st_atime) {
-						ft_strcpy(temp, str[j]);
-						ft_strcpy(str[j], str[j + 1]);
-						ft_strcpy(str[j + 1], temp);
-					}
-				}
+				compare_time_normal_a(file_one, file_two, j, str);
 			}
 			j++;
 		}
