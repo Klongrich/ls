15023ac9d78382188f721275a2813870e6689d01
klongrich <longrichk@gmail.com>
2026-04-12 22:44:58 -0400

    refactoring is_file

--- read_dir/is_file.c
+++ read_dir/is_file.c
@@ -16,34 +16,17 @@ int	is_file(const char *path, t_flags *flags){
 
 	if ((lstat(path, &path_stat)) == -1)
 		return(0);
-	//mode_t file_type = path_stat.st_mode & S_IFMT;
-	//printf("mode_t: %hu\n", file_type);
-	
-	//Character device (e.g. /dev/tty)
-	if (S_ISCHR(path_stat.st_mode)) {
+	if (S_ISCHR(path_stat.st_mode)) 
 		return (1);
-	}
-
-	//Socket file (e.g. /mysql/socket
-	if (S_ISSOCK(path_stat.st_mode)){
+	if (S_ISSOCK(path_stat.st_mode))
 		return (1);
-	}
 	if (is_block_device(path_stat.st_mode))
 		return(1);
 	if(flags->l || flags->color) {
-		//Symoblic Link (e.g. /home -> );
 		if (S_ISLNK(path_stat.st_mode)) {
 			return(1);
 		}
 	}
-
-	//Regular files?
-	/*
-	if (file_type == S_IFREG) {
-		printf("S_IFREG\n");
-		return(1);\
-	}
-	*/
 	return(is_regular_file(path_stat.st_mode));
 
 }
