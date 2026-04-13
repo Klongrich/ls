d481e35faa2b5c9547ae610538f92b89bda85fa6
klongrich <longrichk@gmail.com>
2026-04-13 06:57:22 -0400

    refactoring merge_time_sort

--- sort/merge_time_sort.c
+++ sort/merge_time_sort.c
@@ -1,29 +1,54 @@
 #include "../inc/ft_ls.h"
 
-void	check_time_data(t_merge_data data, int *i, int *j, char *arr[]) {
+int	check_letter(t_merge_data data, int i, int j, char *arr[]) {
+	if(compare_strs_lowercase(data.L[i], data.R[j]) <= 0) {
+		arr[data.k] = data.L[i];
+		return (1);
+	}  else { 
+		arr[data.k] = data.R[j];
+		return(2);
+	}
+}
+
+int	check_time_data(t_merge_data data, int i, int j, char *arr[]) {
 	if (data.right.st_mtime == data.left.st_mtime) {
 		if(data.right.st_mtimespec.tv_nsec == data.left.st_mtimespec.tv_nsec) {
-			if(compare_strs_lowercase(data.L[*i], data.R[*j]) <= 0) 
-				arr[data.k] = data.L[*i++];
-			 else 
-				arr[data.k] = data.R[*j++];
+			return check_letter(data, i, j, arr);
 	 	} else {
-			if (data.right.st_mtimespec.tv_nsec < data.left.st_mtimespec.tv_nsec)
-				arr[data.k] = data.L[*i++];
-			else 
-				arr[data.k] = data.R[*j++];
+			if (data.right.st_mtimespec.tv_nsec < data.left.st_mtimespec.tv_nsec) {
+				arr[data.k] = data.L[i];
+				return (1);
+			} else { 
+				arr[data.k] = data.R[j];
+				return (2);
+			}
 		}
 	} else {
-		if (data.right.st_mtime < data.left.st_mtime)
-			arr[data.k] = data.L[*i++];
-		else 
-			arr[data.k] = data.R[*j++];
+		if (data.right.st_mtime < data.left.st_mtime) {
+			arr[data.k] = data.L[i];
+			return(1);
+		} else { 
+			arr[data.k] = data.R[j];
+			return(2);
+		}
 	}
+	return(0);
+}
+
+void	put_rest_n1(t_merge_data data, t_merge *merge, int i, char *arr[]) {
+	while (i < merge->n1) 
+		arr[data.k++] = data.L[i++];
+}
+
+void    put_rest_n2(t_merge_data data, t_merge *merge, int j, char *arr[]) {
+	while (j < merge->n2)
+		arr[data.k++] = data.R[j++];
 }
 
 void	merge_sides_t(char *arr[], char **L, char **R, t_merge *merge) {
 	int i;
 	int j;
+	int res;
 	t_merge_data data;
 
 	i = 0;
@@ -34,13 +59,15 @@ void	merge_sides_t(char *arr[], char **L, char **R, t_merge *merge) {
 	while (i < merge->n1 && j < merge->n2) {
 		lstat(R[j], &data.right);
 		lstat(L[i], &data.left);
-		check_time_data(data, &i, &j, arr);
+		res = check_time_data(data, i, j, arr);
+		if (res == 1)
+			i++;
+		else if (res == 2)
+			j++;
 		data.k++;
 	}
-	while (i < merge->n1) 
-		arr[data.k++] = L[i++];
-	while (j < merge->n2) 
-		arr[data.k++] = R[j++];
+	put_rest_n1(data, merge, i, arr);
+	put_rest_n2(data, merge, j, arr);
 	free(L);
 	free(R);
 }
@@ -77,7 +104,7 @@ void	init_t_merge_t(t_merge *merge) {
 	merge->k = 0;
 }
 
-void merge_time2(char* arr[], int l, int r, int r_flag) {
+void merge_time_sort(char* arr[], int l, int r, int r_flag) {
 	t_merge merge;
 	int m;
 
@@ -88,8 +115,8 @@ void merge_time2(char* arr[], int l, int r, int r_flag) {
 	merge.r_flag = r_flag;
 	m = l + (r - l) / 2;
 	if (l < r) {
-        	merge_time2(arr, l, m, r_flag);
-        	merge_time2(arr, m + 1, r, r_flag);
+        	merge_time_sort(arr, l, m, r_flag);
+        	merge_time_sort(arr, m + 1, r, r_flag);
         	merge_data_t(arr, &merge);
     	}
 }
